dc7a784f90ddf662d2940289f4c78a310bc81e77
KimSeungHa <bookcon23@gmail.com>
2026-07-02 16:21:07 +0900

    [UI] Drawing Tool 버튼 호버 수정

--- Source/TeamProject/Character/SICharacter.cpp
+++ Source/TeamProject/Character/SICharacter.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "Character/SICharacter.h"
@@ -36,7 +36,7 @@ ASICharacter::ASICharacter()
 	
 	// Fly 상태 Movement 제어값
 	GetCharacterMovement()->BrakingDecelerationFlying = 2000.0f;
-	GetCharacterMovement()->MaxFlySpeed = 800.0f;
+	GetCharacterMovement()->MaxFlySpeed = 2000.0f;
 }
 
 // Called when the game starts or when spawned
